72c745400148d48b04af94bc7275f9f368d94562
Beto Martinez <betoml5>
2020-05-25 16:43:09 -0500

    A little bit of responsive, fixed course feature, still mising the others pages

--- js/index.js
+++
@@ -1,8 +0,0 @@
-const getData = async function(API){
-    const response = await fetch(API);
-    const data = await response.json();
-    console.log(data);
-    return data;
-}
-
-getData('https://api.datos.gob.mx/v1/data-core');
